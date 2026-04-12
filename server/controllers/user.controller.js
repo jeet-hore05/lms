5250e7f317dc826d4c0587b6f4484223f3cc1646
Jeet Hore <jeethore2005@gmail.com>
2026-04-13 00:52:26 +0530

    reset password flow

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -4,6 +4,7 @@ import AppError from "../utils/error.util.js";
 import cloudinary from "cloudinary";
 import fs from "fs/promises";
 import sendEmail from "../utils/sendEmail.js";
+import crypto from "crypto";
 
 const cookieOptions = {
     maxAge : 7*24*60*60*1000,  // 7 days
@@ -177,7 +178,36 @@ const forgotPassword = async (req,res,next)=>{
 };
 
 const resetPassword = async (req,res)=>{
+    const { restToken } = req.params;
 
+    const { password } = req.body;
+
+    const forgotPasswordToken = crypto
+                .createHash("sha256")
+                .update(resetToken)
+                .digest("hex")
+            ;    
+    
+    const user = await User.findOne({
+        forgotPasswordToken,
+        forgotPasswordExpiry : { $gt : Date.now() }
+    })
+
+    if(!user) {
+        return next(new AppError("Token is invalid or expired, please try again.",400));
+    }
+
+    user.password=password;
+
+    user.forgotPasswordToken=undefined;
+    user.forgotPasswordExpiry=undefined;
+
+    user.save();
+
+    res.status(200).json({
+        success:true,
+        message : "Password changed successfully."
+    })
 };
 
 export {

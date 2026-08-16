f732b45156841f8fd1c43457a32dd8419b4c793f
Jeet Hore <jeethore2005@gmail.com>
2026-08-16 11:29:39 +0530

    Added denied page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import Signup from './Pages/Signup.jsx';
 import Login from './Pages/Login.jsx';
 import CourseList from './Pages/Course/CourseList.jsx';
 import Contact from './Pages/Contact.jsx';
+import Denied from './Pages/Denied.jsx';
 
 function App() {
 
@@ -23,6 +24,7 @@ function App() {
           <Route path="/login" element={<Login/>}></Route>
           <Route path="/courses" element={<CourseList/>}></Route>
           <Route path="/contact" element={<Contact/>}></Route>
+          <Route path="/denied" element={<Denied/>}></Route>
 
           <Route path='*' element={<NotFound/>} ></Route>
       </Routes>
